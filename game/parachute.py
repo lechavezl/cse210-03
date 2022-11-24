8c8f121d03e3e79ed477fdd7c894c56ff4f53a82
lechavezl <luisernestochavezleal@gmail.com>
2022-11-23 21:33:40 -0400

    Created attributes and methods for secret_word

---
+++ game/parachute.py
@@ -0,0 +1,20 @@
+class Parachute:
+
+    def __init__(self):
+
+        self._wrong_guesses = 0
+
+    def get_parachute(self):
+        """Create the parachute and display it based on the
+        player's guesses. If the player make a wrong guesses,
+        this method will cut and display the parachute again.
+        """
+
+        pass
+
+    def cut_parachute(self):
+        """Check if the parachue is entirely cut off. If this is true,
+        then end the game.
+        """
+
+        pass
---
+++ game/secret_word.py
@@ -0,0 +1,26 @@
+import random
+
+class SecretWord:
+
+    def __init__(self):
+
+        self._secret_word = ["class", "january", "border", "image", "film", "promise", "kids", "lungs",
+        "doll", "rhyme", "damage", "plants", "dance", "computer", "python", "secret", "plan"]
+        self._random_word = random.choice(self._secret_word)
+
+    def get_world(self):
+        """Get and hold a secret word to display.
+        
+        Args:
+            self (Director): An instance of Director.
+        
+        Returns:
+            string: A random word
+        """
+        
+        word = self._random_word
+        return word
+    
+    def show_secret_world(self):
+
+        pass
